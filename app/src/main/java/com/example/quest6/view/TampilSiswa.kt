44465ac549d2ad13f7f085d18d2444a6405c104a
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-20 12:06:42 +0700

    mengubah code FormIsian di TampilSiswa.kt

--- app/src/main/java/com/example/quest6/view/TampilSiswa.kt
+++ app/src/main/java/com/example/quest6/view/TampilSiswa.kt
@@ -1,104 +1,76 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.quest6.view
 
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quest6.R
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FormIsian(
-    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
-    OnSubmitBtnClick: () -> Unit
+fun TampilData(
+    onBackBtnClick:()->Unit
 ){
-    Scaffold (modifier= Modifier,
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
+    Scaffold(modifier = Modifier,
         {
             TopAppBar(
-                title = {Text(stringResource(id= R.string.home),
-                    color = Color.White)},
-                colors = TopAppBarDefaults.topAppBarColors
-                    (colorResource(id = R.color.teal_700))
-            )}
-    ){ isiRuang ->
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampilan),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource(id=R.color.teal_700)
+                    )
+            )
+        }) { isiRuang->
         Column(modifier = Modifier.padding(isiRuang),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally)
-        {
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .padding(top = 20.dp)
-                    .width(250.dp),
-                label = { Text(text = "Nama Lengkap")},
-                onValueChange = {},
-
-
-                )
-            HorizontalDivider(modifier = Modifier
-                .padding(20.dp)
-                .width(250.dp), thickness = Thickness, color = Color.Red)
-            Row{
-                jenisK.forEach{
-                        item ->
-                    Row(verticalAlignment = Alignment.CenterVertically){
-                        RadioButton(
-                            selected = false,
-                            onClick = {item}
-                        )
-                        Text(text = item)
+            verticalArrangement = Arrangement.SpaceBetween) {
+            Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))) {
+                items.forEach { item ->
+                    Column {
+                        Text(text = item.first.uppercase(),
+                            fontSize = 16.sp)
+                        Text(text = item.second,
+                            fontWeight = FontWeight.Bold,
+                            fontFamily = FontFamily.Cursive, fontSize = 22.sp)
                     }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onBackBtnClick) {
+                    Text(text = stringResource(id=R.string.back))
                 }
             }
-            HorizontalDivider(modifier = Modifier
-                .padding(20.dp)
-                .width(250.dp),
-                thickness = 1.dp,
-                color = Color.Red
-            )
-            OutlinedTextField(
-                value = "",
-                singleLine = true,
-                modifier = Modifier
-                    .width(250.dp),
-                label = { Text(text = "Alamat")},
-                onValueChange = {}
-            )
-            Spacer(modifier = Modifier.height(30.dp))
-            Button(
-                modifier = Modifier.fillMaxWidth(1f),
-                onClick = OnSubmitBtnClick
-            ){
-                Text(stringResource(id = R.string.submit))
-            }
         }
-
     }
 }
-
