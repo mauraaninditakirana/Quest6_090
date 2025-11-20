a4489a66f2220f200a42b101ec580844b8dfffd9
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-20 12:30:52 +0700

    menghapus import yang tidak digunakan di FormSiswa.kt

--- app/src/main/java/com/example/quest6/view/FormSiswa.kt
+++ app/src/main/java/com/example/quest6/view/FormSiswa.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
